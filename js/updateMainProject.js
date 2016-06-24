10e104751bbf52518604a6deaa5928e4a4210644
joaosmaraujoESEIG <9140322@eseig.ipp.pt>
2016-06-24 20:23:18 +0100

    first commit

---
+++ js/updateMainProject.js
@@ -0,0 +1,68 @@
+$("#infradash-info").click(function(){
+    var infradash = '<div class="col-md-8">';
+    infradash = infradash + '<img class="img-responsive" src="images/infradash/1_infradash_home.jpg" alt="">';
+    infradash = infradash + '</div>';
+    infradash = infradash + '<div class="col-md-4">';
+    infradash = infradash + '<h3>Geojustiça Website<br><small>Schedule: March 2016 to June 2016</small></h3>';
+    infradash = infradash + '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>';
+    infradash = infradash + '<br>';
+    infradash = infradash + '<h4>Languages and Tecnhologies</h4>';
+    infradash = infradash + '<ul>';
+    infradash = infradash + '<li><b>Frontend: </b>HTML5, CSS3, Bootstrap, Javascript, jQuery</li>';
+    infradash = infradash + '<li><b>Backend: </b>PHP, Laraval 5.2</li>';
+    infradash = infradash + '<li><b>Database: </b>PostgreSQL</li>';
+    infradash = infradash + '<li><b>Other Relevant Stuff: </b>jQuery AJAX and some Javascript data presentations libraries as ChartJS and ECharts</li>';
+    infradash = infradash + '</ul>';
+    infradash = infradash + '<br>';
+    infradash = infradash + '<a class="btn btn-primary" href="infradash.html">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>';
+    infradash = infradash + '</div>';
+    
+    $("#main-project").html(infradash);
+    
+});
+
+
+
+$("#geojustica-info").click(function(){
+    var geojustica = '<div class="col-md-8">';
+    geojustica = geojustica + '<img class="img-responsive" src="images/geojustica/1_geojustica_home.jpg" alt="">';
+    geojustica = geojustica + '</div>';
+    geojustica = geojustica + '<div class="col-md-4">';
+    geojustica = geojustica + '<h3>Geojustiça Website<br><small>Schedule: December 2015 to January 2015</small></h3>';
+    geojustica = geojustica + '<p>The Geojustiça website was the last project I developed for digisensei. I was the only responsible for designing all the layouts and for coding it. It was developed using LocomotiveCMS, an open source CMS, and the common Frontend technologies referred above.</p>';
+    geojustica = geojustica + '<br>';
+    geojustica = geojustica + '<h4>Languages and Tecnhologies</h4>';
+    geojustica = geojustica + '<ul>';
+    geojustica = geojustica + '<li><b>Frontend: </b>HTML5, SCSS, Bootstrap, Javascript, jQuery</li>';
+    geojustica = geojustica + '<li><b>Backend: </b>LocomotiveCMS</li>';
+    geojustica = geojustica + '<li><b>Other Relevant Stuff: </b>Google Maps Javascript API</li>';
+    geojustica = geojustica + '</ul>';
+    geojustica = geojustica + '<br>';
+    geojustica = geojustica + '<a class="btn btn-primary" href="geojustica.html">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>';
+    geojustica = geojustica + '</div>';
+    
+    $("#main-project").html(geojustica);
+});
+
+
+
+$("#verdegosto-info").click(function(){
+    var verdegosto = '<div class="col-md-8">';
+    verdegosto = verdegosto + '<img class="img-responsive" src="images/verdegosto/verdegosto_home.jpg" alt="">';
+    verdegosto = verdegosto + '</div>';
+    verdegosto = verdegosto + '<div class="col-md-4">';
+    verdegosto = verdegosto + '<h3>Geojustiça Website<br><small>Schedule: November 2015 to December 2015</small></h3>';
+    verdegosto = verdegosto + '<p>Verde Gosto is the official website of a wines company which was developed by digisensei. As part of their team I was responsible for coding some of the website layouts.</p>';
+    verdegosto = verdegosto + '<br>';
+    verdegosto = verdegosto + '<h4>Languages and Tecnhologies</h4>';
+    verdegosto = verdegosto + '<ul>';
+    verdegosto = verdegosto + '<li><b>Frontend: </b>HTML5, SCSS, Bootstrap, Javascript, jQuery</li>';
+    verdegosto = verdegosto + '<li><b>Backend: </b>LocomotiveCMS</li>';
+    verdegosto = verdegosto + '<li><b>Other Relevant Stuff: </b>Google Maps Javascript API</li>';
+    verdegosto = verdegosto + '</ul>';
+    verdegosto = verdegosto + '<br>';
+    verdegosto = verdegosto + '<a class="btn btn-primary" href="http://www.verdegosto.com/" target="_blank">View Project <span class="glyphicon glyphicon-chevron-right"></span></a>';
+    verdegosto = verdegosto + '</div>';
+    
+    $("#main-project").html(verdegosto);
+});
